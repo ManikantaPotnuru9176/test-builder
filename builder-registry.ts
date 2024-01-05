5db648f0d0488b595d2256e67b22b6ba98a91442
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-01-05 15:15:27 +0530

    chore: removed the components

--- builder-registry.ts
+++ builder-registry.ts
@@ -1,18 +1,2 @@
 "use client";
 import { Builder } from "@builder.io/react";
-import Counter from "./components/Counter/Counter";
-import LandingPage from "./components/LandingPage";
-
-Builder.registerComponent(Counter, {
-  name: "Counter",
-  inputs: [
-    {
-      name: "initialCount",
-      type: "number",
-    },
-  ],
-});
-
-Builder.registerComponent(LandingPage, {
-  name: "LandingPage",
-});
